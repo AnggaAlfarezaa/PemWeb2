8f5a8a66d49ccb83df80b4dc48299ad03e46c2bc
Angga Alfareza <angg23185ti@student.nurulfikri.ac.id>
2024-03-14 22:31:22 +0700

    pertemuan 3

---
+++ praktikum3/data.php
@@ -0,0 +1,26 @@
+<?php 
+
+$prodi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital",
+];
+$skills = [
+    "HTML" => 10,
+    "CSS" => 10,
+    "JavaScript" => 20,
+    "RwD Bootstrap" => 20,
+    "PHP" => 30,
+    "Phyton" => 30,
+    "Java" => 50,
+];
+$domisili = [
+    "Jakarta",
+    "Bogor",
+    "Depok",
+    "Tangerang",
+    "Bekasi",
+    "Lainnya"
+];
+
+?>
